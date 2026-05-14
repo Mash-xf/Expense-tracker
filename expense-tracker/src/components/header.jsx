a9300aea4a669c6d2d9615819aabaf93fcf514a2
mash-xf <felix.macharia@student.moringaschool.com>
2026-05-14 13:35:56 +0300

    header

---
+++ expense-tracker/src/components/header.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+function header() {
+    return (
+        <header className='header'>
+            <h1>Expense Tracker</h1>
+            <span className='tagline'>Track your expenses and income</span>
+            <nav className='nav'>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/expenses">Expenses</NavLink>
+            <NavLink to="/income">Income</NavLink>
+            </nav>      
+        </header>
+    )
+}
